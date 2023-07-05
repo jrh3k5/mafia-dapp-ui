452226de3567d4e00da790f090e9c8c37efc863f
Joshua Hyde <jrh3k5@gmail.com>
2023-07-05 13:08:29 -0500

    Begin implementing hosting UI

---
+++ src/components/HostGame.vue
@@ -0,0 +1,31 @@
+<script>
+import * as game from '../js/game.js'
+import * as errors from '../js/errors.js'
+
+export default {
+  methods: { },
+  mounted() {
+    game.initializeGame(this.gameState).then(() => {
+      this.gameInitialized = true;
+    }).catch(err => {
+        errors.reportError("failed to initialize game", err);
+    })
+  }
+}
+</script>
+
+<script setup>
+    defineProps(["gameState"])
+</script>
+
+<template>
+  <div v-if="this.gameInitialized === true">
+    You are hosting a game! When everyone has joined, enter the number of users you are expecting and click 'Start Game':
+
+    <p />
+
+    <input type="text" />
+    <br />
+    <button type="submit" on-click="console.log('click click!')">Start Game</button>
+  </div>
+</template>
--- src/js/game.js
+++ src/js/game.js
@@ -11,7 +11,9 @@ export function setMafiaContract(gameState, mafiaContract) {
 // initializeGame is used to initialize a game. It returns a Promise that either returns the error or resolves to no-arg completion.
 export function initializeGame(gameState) {
     return new Promise((resolve, reject) => {
-        gameState.mafiaContract.methods.initializeGame(gameState.userWalletAddress).call((err, res) => {
+        console.log("executing");
+        gameState.mafiaContract.methods.initializeGame(gameState.userWalletAddress).send({ from: gameState.userWalletAddress }, (err, txHash) => {
+            console.log("executed");
             if (err) {
                 reject(err);
             } else {
