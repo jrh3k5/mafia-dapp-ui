2f73573955cdb163f14272cefd19f6f1328a8807
Joshua Hyde <jrh3k5@gmail.com>
2023-07-07 08:28:44 -0500

    add storage of state

--- src/js/game_state.js
+++ src/js/game_state.js
@@ -2,28 +2,34 @@ const STATE_HOSTING = "hosting"
 const STATE_JOINING = "joining"
 const STATE_WALLET_CONNECTED = "wallet-connected"
 
-let gameState;
+let gameState = null;
 
 // resetGameState resets the game state to a default state
 // It does not clear out the game state, so required initial values (such as user wallet address) are retained.
 export function resetGameState() {
-    if(!gameState) {
-        return;
+    localStorage.removeItem("game-state");
+    if(gameState) {
+        gameState = initializeGameState(gameState.getUserAddress());
     }
-
-    gameState = initializeGameState(gameState.getUserAddress());
 }
 
 export function getGameState() {
-    if (!gameState) {
-        throw "game state not initialized";
+    if (gameState) {
+        return gameState;
     }
 
-    return gameState;
+    const newState = new GameState();
+    if (newState.hydrateFromStorage()) {
+        gameState = newState;
+        return newState;
+    }
+
+    throw "game state not initialized";
 }
 
 export function initializeGameState(userAddress) {
-    gameState = new GameState(userAddress);
+    const gameState = new GameState(userAddress);
+    gameState.toStorage();
     return gameState;
 }
 
@@ -44,19 +50,48 @@ class GameState {
         return this.userAddress;
     }
 
+    // hydrateFromStorage attempts to hydrate this instance from localStorage.
+    // It returns true if storage data was found and this has been hydrated; it returns false, otherwise.
+    hydrateFromStorage() {
+        const storedState = localStorage.getItem("game-state");
+        if (!storedState) {
+            return false;
+        }
+
+        const restoredState = JSON.parse(storedState);
+        Object.assign(this, restoredState);
+        return true;
+    }
+
     isHosting() {
         return this.hosting;
     }
 
     setHostAddress(hostAddress) {
         this.hostAddress = hostAddress;
+        this.toStorage();
     }
 
     setIsHosting(hosting) {
         this.hosting = hosting;
+        this.toStorage();
     }
 
     setPlayerAddress(playerAddress) {
         this.playerAddress = playerAddress;
+        this.toStorage();
+    }
+
+    toStorage() {
+        const toStore = {
+            hostAddress: this.hostAddress,
+            hosting: this.hosting,
+            playerAddress: this.playerAddress,
+            userAddress: this.userAddress,
+        }
+
+        const stateString = JSON.stringify(toStore);
+
+        localStorage.setItem("game-state", stateString);
     }
 }
