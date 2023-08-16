6fee3d085020ce2c4c7363ac7575976cb2674096
Joshua Hyde <jrh3k5@gmail.com>
2023-08-15 19:07:56 -0500

    Add error handling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script>
+import { addErrorHandler } from './js/errors.js'
 import './css/styles.css'
 
+export default {
+  mounted() {
+    addErrorHandler((msg, err) => {
+      this.errorMessage = msg;
+      if (err) {
+        console.error(err);
+      }
+      return true;
+    })
+  },
+  data(){
+    return {
+      errorMessage: null,
+    }
+  }
+}
 </script>
 
 <template>
   <div id="main-content">
-    <router-view></router-view>
+    <div v-if="this.errorMessage" class="error">
+      {{ this.errorMessage }}
+    </div>
+    <div id="router-view">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
--- src/js/errors.js
+++ src/js/errors.js
@@ -1,4 +1,22 @@
+let errorHandlers = [];
+
+// addErrorHandler adds the given handler function to the error handlers used when an error is reported.
+// The function accepts a string and an optional error object as its parameters and returns true 
+// if the error has been handled.
+export function addErrorHandler(handlerFn) {
+    errorHandlers.push(handlerFn)
+}
+
+// reportError is used to report an error; the first parameter is the human-readable
+// message to show, and the second is an optional error object.
 export function reportError(msg, err) {
+    for (let i = 0; i < errorHandlers.length; i++) {
+        if (errorHandlers[i](msg, err) === true) {
+            // there's a registered handler that handled the issue; don't do the fallback
+            return
+        }
+    }
+
     if(err) {
         console.error(err);
     }
